7c037639185e2a35bfad7da9bbe17116da583902
Tomer <gelbharttomer@gmail.com>
2025-10-21 12:44:42 +0100

    Update API to use NASA TAP service

--- backend/src/services/nasaService.js
+++ backend/src/services/nasaService.js
@@ -1,19 +1,25 @@
 import axios from 'axios';
 
-const NASA_EXOPLANET_API = 'https://exoplanetarchive.nasa.gov/cgi-bin/nstedAPI/nph-nstedAPI';
+const NASA_TAP_API = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync';
 
 class NasaService {
   async getExoplanets(params = {}) {
     try {
-      const queryParams = {
-        table: 'exoplanets',
-        format: 'json',
-        select: 'pl_name,hostname,discoverymethod,disc_year,pl_rade,pl_bmasse,sy_dist,pl_orbper,pl_eqt,pl_dens',
-        ...params
-      };
+      let query = `select pl_name,hostname,discoverymethod,disc_year,pl_rade,pl_bmasse,sy_dist,pl_orbper,pl_eqt,pl_dens from ps where default_flag=1`;
 
-      const response = await axios.get(NASA_EXOPLANET_API, {
-        params: queryParams
+      if (params.where) {
+        query += ` and ${params.where}`;
+      }
+
+      if (params.order) {
+        query += ` order by ${params.order}`;
+      }
+
+      const response = await axios.get(NASA_TAP_API, {
+        params: {
+          query: query,
+          format: 'json'
+        }
       });
 
       return response.data;
@@ -25,15 +31,13 @@ class NasaService {
 
   async searchExoplanets(searchTerm) {
     try {
-      const queryParams = {
-        table: 'exoplanets',
-        format: 'json',
-        select: 'pl_name,hostname,discoverymethod,disc_year,pl_rade,pl_bmasse,sy_dist,pl_orbper,pl_eqt,pl_dens',
-        where: `pl_name like '%${searchTerm}%' or hostname like '%${searchTerm}%'`
-      };
+      const query = `select pl_name,hostname,discoverymethod,disc_year,pl_rade,pl_bmasse,sy_dist,pl_orbper,pl_eqt,pl_dens from ps where default_flag=1 and (pl_name like '%${searchTerm}%' or hostname like '%${searchTerm}%')`;
 
-      const response = await axios.get(NASA_EXOPLANET_API, {
-        params: queryParams
+      const response = await axios.get(NASA_TAP_API, {
+        params: {
+          query: query,
+          format: 'json'
+        }
       });
 
       return response.data || [];
